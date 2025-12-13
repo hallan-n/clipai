ffd4dc3c6a3a1c504dd73ffe2135cac5ad5d97a8
hallan-n <hallanneves01@gmail.com>
2025-12-13 18:31:31 -0300

    Adicionado extração do texto

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,13 @@
-FROM python:3.11-slim
+FROM python:3.12-slim
 
-# Instala dependências do sistema necessárias
-RUN apt-get update && apt-get install -y \
-    git \
-    build-essential \
-    libffi-dev \
-    libssl-dev \
-    python3-dev \
-    wget \
-    && rm -rf /var/lib/apt/lists/*
+# Instalar ffmpeg e dependências
+RUN apt-get update && apt-get install -y ffmpeg && rm -rf /var/lib/apt/lists/*
 
-# Define diretório de trabalho
 WORKDIR /app
 
-# Copia código e requirements
-COPY ./app /app
-COPY requirements.txt /app/requirements.txt
-
-# Atualiza pip
-RUN pip install --upgrade pip
-
-# Instala Torch CPU-only com index especial
-RUN pip install torch==2.9.1+cpu torchvision==0.24.1+cpu torchaudio==2.9.1+cpu --index-url https://download.pytorch.org/whl/cpu
-
-# Instala o restante das dependências (Whisper e Loguru)
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Executa o main.py por padrão
-CMD ["python", "main.py"]
+COPY app ./app
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,50 @@
-import whisper
+from faster_whisper import WhisperModel
+import subprocess
 
-model = whisper.load_model("base")
-result = model.transcribe("./data/audio.wav")
-print(result["text"])
+model = WhisperModel(
+    "tiny",
+    device="cpu",
+    compute_type="int8"
+)
+
+def get_transcribe(path: str) -> list[dict]:
+    segments, _ = model.transcribe(path, language="pt")
+
+    return [
+        {
+            "text": seg.text.strip(),
+            "start": seg.start,
+            "end": seg.end
+        }
+        for seg in segments
+    ]
+
+
+
+def extract_audio(video_path: str, audio_path: str):
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-y",
+            "-i", video_path,
+            "-vn",
+            "-ac", "1",
+            "-ar", "16000",
+            audio_path
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        check=True
+    )
+
+
+if __name__ == "__main__":
+    from datetime import datetime, timedelta
+
+    start = datetime.now()
+    extract_audio("./video.mp4", "./audio.wav")
+    get_transcribe("./audio.wav")
+
+    end = datetime.now()
+
+    print((end - start).total_seconds())
